b93603052ce1117772053f043530603bd3d07668
jamiewallace <wallacejamiec@gmail.com>
2013-11-01 09:37:14 +0000

    about to push to Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Course.destroy_all
 Room.destroy_all
 User.destroy_all
 
-course1 = Course.create(title: "UX for beginners", short_desc: "spmf spofj psdojf ", long_desc: "Vestibulum convallis feugiat venenatis. Mauris vulputate ligula sit amet dignissim tincidunt. Morbi suscipit urna vel placerat euismod. Sed ut rutrum turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce hendrerit lectus a sapien facilisis luctus et a turpis. Suspendisse sed scelerisque orci, at tempus lorem. Mauris rutrum eros vel turpis auctor, at gravida lectus posuere. Aenean vel tempus orci, a hendrerit lacus.", cost: 1400 , time_commit: 4, start_date: "12/20/2013" , end_date: "02/20/2014", start_time: Time.parse("09:00"), end_time: Time.parse("18:00") )
+course1 = Course.create(title: "UX for beginners", short_desc: "spmf spofj psdojf ", long_desc: "Vestibulum convallis feugiat venenatis. Mauris vulputate ligula sit amet dignissim tincidunt. Morbi suscipit urna vel placerat euismod. Sed ut rutrum turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce hendrerit lectus a sapien facilisis luctus et a turpis. Suspendisse sed scelerisque orci, at tempus lorem. Mauris rutrum eros vel turpis auctor, at gravida lectus posuere. Aenean vel tempus orci, a hendrerit lacus.", cost: 1400 , time_commit: 4, start_date: "12/20/2013" , end_date: "02/20/2014", start_time: Time.parse("09:00"), end_time: Time.parse("18:00"))
 course2 = Course.create(title: "Ruby for beginners", short_desc: "smf f pofd dfkl", long_desc: "Vestibulum convallis feugiat venenatis. Mauris vulputate ligula sit amet dignissim tincidunt. Morbi suscipit urna vel placerat euismod. Sed ut rutrum turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce hendrerit lectus a sapien facilisis luctus et a turpis. Suspendisse sed scelerisque orci, at tempus lorem. Mauris rutrum eros vel turpis auctor, at gravida lectus posuere. Aenean vel tempus orci, a hendrerit lacus.", cost: 8000 , time_commit: 12, start_date: "08/10/2013" , end_date: "11/10/2013", start_time: Time.parse("09:00"), end_time: Time.parse("18:00"))
 #course3 = Course.create(title: "Full stack development", short_desc: "pof fok o a[fok ", long_desc: "Vestibulum convallis feugiat venenatis. Mauris vulputate ligula sit amet dignissim tincidunt. Morbi suscipit urna vel placerat euismod. Sed ut rutrum turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce hendrerit lectus a sapien facilisis luctus et a turpis. Suspendisse sed scelerisque orci, at tempus lorem. Mauris rutrum eros vel turpis auctor, at gravida lectus posuere. Aenean vel tempus orci, a hendrerit lacus.", :cost , :time_commit , :start_date, :end_date, :start_time, :end_time, :recurrence, :room_id)
 #course4 = Course.create(title: "Front end development", short_desc: "apsdfk [oksf kf d", long_desc: "Vestibulum convallis feugiat venenatis. Mauris vulputate ligula sit amet dignissim tincidunt. Morbi suscipit urna vel placerat euismod. Sed ut rutrum turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce hendrerit lectus a sapien facilisis luctus et a turpis. Suspendisse sed scelerisque orci, at tempus lorem. Mauris rutrum eros vel turpis auctor, at gravida lectus posuere. Aenean vel tempus orci, a hendrerit lacus.", :cost , :time_commit , :start_date, :end_date, :start_time, :end_time, :recurrence, :room_id)
@@ -28,6 +28,9 @@ room4 = Room.create(name: "Room4", capacity: 8)
 
 course1.room = room1
 course1.save
+
+course2.room = room2
+course2.save
 
 user1.courses = [course1, course2]
 user1.save
